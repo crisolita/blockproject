995e0ea8247b042f39a3e6ea21258efda281a217
Crisol Cova <crisolvalentian@gmail.com>
2024-01-04 11:31:47 -0400

    stripe prod

--- controllers/stripe.ts
+++ controllers/stripe.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { getUserById, updateUser } from "../service/user";
 const stripe = require("stripe")(process.env.SK_TEST);
-const endpointSecret = process.env.WEBHOOKSECRET_TEST;
 export const onboardLink = async (req: Request, res: Response) => {
   try {
     // @ts-ignore
@@ -68,57 +67,4 @@ export const validateDataOnboarding = async (req: Request, res: Response) => {
     console.log(e);
     res.status(500).json({ error: e });
   }
-};
-export const webhookControler = (req: Request, res: Response) => {
-  let event;
-
-  // Verify the event came from Stripe
-  try {
-    const sig = req.headers["stripe-signature"];
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-  } catch (err: any) {
-    // On error, log and return the error message
-    console.log(`❌ Error message: ${err.message}`);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
-  let ans;
-  // Successfully constructed event
-  switch (event.type) {
-    case "identity.verification_session.verified": {
-      // All the verification checks passed
-      const verificationSession = event.data.object;
-      ans = verificationSession;
-      break;
-    }
-    case "identity.verification_session.requires_input": {
-      // At least one of the verification checks failed
-      const verificationSession = event.data.object;
-      ans = verificationSession;
-
-      console.log(
-        "Verification check failed: " + verificationSession.last_error.reason
-      );
-
-      // Handle specific failure reasons
-      switch (verificationSession.last_error.code) {
-        case "document_unverified_other": {
-          // The document was invalid
-          break;
-        }
-        case "document_expired": {
-          // The document was expired
-          break;
-        }
-        case "document_type_not_supported": {
-          // document type not supported
-          break;
-        }
-        default: {
-          // ...
-        }
-      }
-    }
-  }
-
-  res.json({ received: true, verificationSession: ans });
 };
--- routes/stripe.ts
+++ routes/stripe.ts
@@ -1,19 +1,11 @@
 import express from "express";
 import { authenticateToken } from "../middleware/auth";
-import {   onboardLink, validateDataOnboarding, webhookControler } from "../controllers/stripe";
+import { onboardLink, validateDataOnboarding } from "../controllers/stripe";
 import bodyParser from "body-parser";
 
 const router = express.Router();
 // RECUERDA PONER LOS VALIDADORES DE JOI
 router.post("/onBoard", authenticateToken, onboardLink);
-router.post("/onboard-user-return",authenticateToken,validateDataOnboarding)
-
-
-
-
-
-
-
-
+router.post("/onboard-user-return", authenticateToken, validateDataOnboarding);
 
 export default router;
