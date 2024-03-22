36e0adc743809a68a4625f35ffff95b3bc8f1fae
Crisol Cova <crisolvalentian@gmail.com>
2024-03-22 10:56:10 -0400

    new endpoint

--- routes/entradas.ts
+++ routes/entradas.ts
@@ -1,29 +1,40 @@
 import express from "express";
 import { authenticateToken } from "../middleware/auth";
-import {  canjearNFTporEntada, canjearNFTporEntadaTEST, getEntradas, validarEntrada } from "../controllers/entradas";
+import {
+  canjearNFTporEntada,
+  canjearNFTporEntadaTEST,
+  getEntradas,
+  updateEntrada,
+  validarEntrada,
+} from "../controllers/entradas";
 import Joivalidator from "express-joi-validation";
-import { querySchemaCanjeoDeEntrada, querySchemaDorsal, querySchemaValidarEntrada } from "../middleware/validation";
+import {
+  querySchemaCanjeoDeEntrada,
+  querySchemaDorsal,
+  querySchemaValidarEntrada,
+} from "../middleware/validation";
 
 const validator = Joivalidator.createValidator();
 
 const router = express.Router();
 // RECUERDA PONER LOS VALIDADORES DE JOI
 
-router.post("/canjeoEntrada",validator.body(querySchemaCanjeoDeEntrada),authenticateToken,canjearNFTporEntada)
-router.post("/canjeoTEST",canjearNFTporEntadaTEST)
+router.post(
+  "/canjeoEntrada",
+  validator.body(querySchemaCanjeoDeEntrada),
+  authenticateToken,
+  canjearNFTporEntada
+);
+router.post("/canjeoTEST", canjearNFTporEntadaTEST);
 
-router.post("/validar",authenticateToken,validator.body(querySchemaValidarEntrada),validarEntrada)
+router.post(
+  "/validar",
+  authenticateToken,
+  validator.body(querySchemaValidarEntrada),
+  validarEntrada
+);
+router.post("/updateEntrada", authenticateToken, updateEntrada);
 
-
-
-router.get("/", authenticateToken,getEntradas);
-
-
-
-
-
-
-
-
+router.get("/", authenticateToken, getEntradas);
 
 export default router;
